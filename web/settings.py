d617ee3d76095dd4cd9da5c74a6dbfd1ce9ef169
Seth Adam Denner <seth.adam.denner@gmail.com>
2012-06-06 00:48:06 -0700

    Admin site working

--- web/settings.py
+++ web/settings.py
@@ -118,12 +118,12 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
