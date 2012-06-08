a7b83f7704b64694f7e08679dc4fe620a4bbf275
U-fuck-PC\fuck <fuck@sethdenner-desktop.(none)>
2012-06-07 21:22:24 -0700

    Admin tool working

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from knotis.models.accounts import Currency, AccountType, Account 
+from models.knotis.accounts import Currency, AccountType, Account 
 admin.site.register(Currency);
 admin.site.register(AccountType);
 admin.site.register(Account);
 
-from knotis.models.businesses import Business
+from models.knotis.businesses import Business
 admin.site.register(Business)
