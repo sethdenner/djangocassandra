d2d0e64ae3f94e321ac709b2e941dba3262dcfce
Seth Denner <seth@boundary-bay.com>
2012-07-06 18:25:54 -0700

    jQuery render_data plugin. First functional draft. Time for testing/debugging.

--- web/app/static/scripts/jquery/plugins/render_data.js
+++ web/app/static/scripts/jquery/plugins/render_data.js
@@ -27,6 +27,7 @@
  */
 
 (function($) {
+    var DATA_PREFIX = 'data-';
     var _options = null; //key/value collection of settings parsed from dom.
     
     var _get_data = function($template, attributes) {
@@ -36,31 +37,34 @@
             url: attributes.resource_uri,
             dataType: 'json',
             success: function(data, status, jq_xhr) {
+                var data_name = DATA_PREFIX + 'data';
                 $template.attr({
-                  'data-data': data      
+                  data_name: data      
                 });
                 $template.render_data();
             }
         });
-    }
+    };
     
     var _get_attributes = function(element) {
         if (null === element) { return null; }
         
-        attributes = new Object();
-        element_attributes = element.attributes;
-        attributes_length = element_attributes.length;
+        var attributes = {};
+        var element_attributes = element.attributes;
+        var attributes_length = element_attributes.length;
         for (var i = 0; i < attributes_length; ++i) {
-            attribute = element_attributes.item(i);
-            if (attribute.nodeName.indexOf('data-') != 0) { continue; }
+            var attribute = element_attributes.item(i);
+            var attribute_node_name = attribute.nodeName;
+            if (attribute_node_name.indexOf(DATA_PREFIX) !== 0) { continue; }
             
-            $.extend({
-                attribute.nodeName: attribute:nodeValue
+            var name = attribute_node_name.substr(DATA_PREFIX.length);
+            attributes = $.extend({
+                name: attribute.nodeValue
             }, attributes);
         }
         
         return attributes;
-    }
+    };
     
     jQuery.fn.render_data = function(options) {
         _options = $.extend({
@@ -68,20 +72,20 @@
         }, options);
         
         return this.each(function (){
-            attributes = _get_attributes(this);
+            var attributes = _get_attributes(this);
 
-            if (null === attributes.data || true == _options.force_api) {
+            if (null === attributes.data || true === _options.force_api) {
                 _get_data(this, attributes);
                 return true;
             }
             
-            data = JSON.parse(attributes.data);
+            var data = JSON.parse(attributes.data);
             
-            $template = this;
+            var $template = this;
             $.each(data, function(key, value){
                 var $elements = $template.find('.' + key);
                 $elements.each(function(){
-                    $this = $(this);
+                    var $this = $(this);
                     if ($this.is('input')) {
                         $this.attr({
                             'name': key,
