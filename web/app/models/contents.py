9d180a6b38efffe40b04a9189d74168f636049f5
Seth Adam Denner <seth.adam.denner@gmail.com>
2012-07-16 14:19:43 -0700

    Cleaned up contents model

--- web/app/models/contents.py
+++ web/app/models/contents.py
@@ -1,9 +1,8 @@
+from app.models.fields.permissions import PermissionsField
+from app.models.knotis import KnotisModel
 from django.contrib.auth.models import User, Group
 from django.db.models import CharField, ForeignKey, DateTimeField, FloatField
-
-from app.models.knotis import KnotisModel
-from app.models.fields.binary import Base64Field
-from app.models.fields.permissions import PermissionsField
+from django.utils.datetime_safe import datetime
 
 
 class ContentType(KnotisModel):
@@ -12,18 +11,22 @@ class ContentType(KnotisModel):
         ('1', 'site text'),
         ('2', 'user submitted')
         # terms of service
-        # hours, address, business name, all endpoints, anything that a user can say about a business.
+        # hours, address, business name, all endpoints, anything that a user
+        # can say about a business.
     )
 
-    value       = CharField(max_length=30, choices=CONTENT_TYPES)
+    value = CharField(max_length=30, choices=CONTENT_TYPES)
     permissions = PermissionsField()
 
     pub_date = DateTimeField('date published')
+
     def __unicode__(self):
         return self.value
 
+
 class Content(KnotisModel):
-#    FIXME: how do we make this auto version so that save actually just increments a version to the list but we always have old versions?
+#    FIXME: how do we make this auto version so that save actually just
+#    increments a version to the list but we always have old versions?
 #    cid = content id.
 #    parent_id(indexed)
 #    owner_id(indexed)
@@ -41,20 +44,19 @@ class Content(KnotisModel):
     #content_previous = ForeignKey('self')
 
     c_type = ForeignKey(ContentType)
-    value = CharField(max_length=2000) #Base64Field()
+    value = CharField(max_length=2000)  # Base64Field()
 
-    certanty_mu = FloatField() # average - expected value
-    certanty_sigma = FloatField() # stdev - expected error
+    certanty_mu = FloatField()  # average - expected value
+    certanty_sigma = FloatField()  # stdev - expected error
 
     pub_date = DateTimeField('date published')
-    
+
     def __unicode__(self):
         return self.value
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= datetime.now() - datetime.timedelta(days=1)
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-
