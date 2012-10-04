ec59aeede2ac3122f7e0f7d716bfe81341dfb6b0
Seth Denner <seth.adam.denner@gmail.com>
2012-10-04 16:18:15 -0700

    Old Site Port
    Hamburgertime
    
    http://www.youtube.com/watch?v=msGvEtmR970&feature=youtube_gdata

--- web/legacy/authentication/backends.py
+++ web/legacy/authentication/backends.py
@@ -1,5 +1,59 @@
 from knotis_auth.models import User
 
+
+class HamburgertimeAuthenticationBackend(object):
+    '''
+    This is a terrible solution to the problem of needing management to be
+    able to edit merchants information.
+
+    This should be replaced with a proper admin too ASAP!
+
+    If you are reading this message please contact your manager IMMEDIATELY
+    and DEMAND to know why the fuck this is still live in production.
+
+    Thanks,
+
+    Seth Denner
+    10/04/2012
+    '''
+    HAMBURGERTIME_USERNAME = '__hamburgertime__'
+
+    def authenticate(
+        self,
+        username=None,
+        password=None
+    ):
+        try:
+            hamburgertime = User.objects.get(
+                username=self.HAMBURGERTIME_USERNAME
+            )
+
+        except:
+            hamburgertime = None
+
+        if None == hamburgertime:
+            return None
+
+        if not hamburgertime.check_password(password):
+            return None
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            user = None
+
+        return user
+
+    def get_user(
+        self,
+        user_id
+    ):
+        try:
+            return User.objects.get(pk=user_id)
+        except:
+            return None
+
+
 class LegacyAuthenticationBackend(object):
     def authenticate(
         self,
@@ -11,14 +65,14 @@ class LegacyAuthenticationBackend(object):
 
         except User.DoesNotExist:
             return None
-                
-        if not user.check_pasword(password):
+
+        if not user.check_password(password):
             return None
-        
+
         user.set_password(password)
         user.save()
         return user
-            
+
     def get_user(
         self,
         user_id
@@ -27,4 +81,3 @@ class LegacyAuthenticationBackend(object):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-        
