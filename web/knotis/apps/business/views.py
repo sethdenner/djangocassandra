f7f4dd3a8ddd9d23ad0968d408f4719244895ecd
Seth Denner <seth.adam.denner@gmail.com>
2012-10-24 17:04:18 -0700

    Sickle
    Integrated image cropping with offers.

--- web/knotis/apps/business/views.py
+++ web/knotis/apps/business/views.py
@@ -136,48 +136,6 @@ def set_primary_image(
 
 
 @login_required
-def delete_image(
-    request,
-    business_id,
-    image_id
-):
-    if request.method.lower() != 'post':
-        return HttpResponseBadRequest('Method must be post.')
-
-    try:
-        business = Business.objects.get(pk=business_id)
-
-    except:
-        business = None
-
-    if not business:
-        return HttpResponseNotFound('Business not found.')
-
-    if business.user_id != request.user.id:
-        return HttpResponseBadRequest('Business does not belong to logged in user!')
-
-    try:
-        image = Image.objects.get(pk=image_id)
-
-    except:
-        image = None
-
-    if not image:
-        return HttpResponseNotFound('Image not found')
-
-    if business.id != image.related_object_id:
-        return HttpResponseBadRequest('Image does not belong to business!')
-
-    try:
-        image.delete()
-
-    except Exception, error:
-        return HttpResponseServerError(error)
-
-    return HttpResponse('OK')
-
-
-@login_required
 def edit_profile(request):
     update_form = None
     link_form = None
@@ -257,9 +215,11 @@ def edit_profile(request):
     try:
         images = Image.objects.filter(related_object_id=business.id)
         options = {
+            'alt_text': business.business_name.value,
             'images': images,
             'business': business,
-            'dimensions': '35x19'
+            'dimensions': '35x19',
+            'image_class': 'business-gallery'
         }
         template_parameters['image_list'] = render_image_list(options)
     except:
--- web/knotis/static/scripts/views/offer.edit.js
+++ web/knotis/static/scripts/views/offer.edit.js
@@ -13,5 +13,60 @@ $(function() {
             
         }
     });
-    
+
+    offer_id = $('#update_offer').attr('data-id');
+    var init_cropping = function() {
+        $('.crop').sickle({
+            aspect: 1,
+            done: function(data) {
+                if (data.status == 'success') {
+                    $.get([
+                        '/image/get_row/',
+                        data.image_id,
+                        '/'].join(''), function(data){
+                        $('#image-list').html(data);
+                        init_cropping();    
+                    
+                    })
+                    
+                } else if (data.status == 'failure') {
+                    
+                } else {
+                    // Invalid Status
+                }
+            }
+            
+        });
+        
+    };
+    init_cropping();
+
+    $('#file-uploader').sickle({
+        do_upload: true,
+        params: {
+            id: offer_id ? offer_id : null,
+            type: 'image'
+        },
+        aspect: 1,
+        done: function(data) {
+            if (data.status == 'success') {
+                $('#image-id').val(data.image_id);
+                $.get([
+                    '/image/get_row/',
+                    data.image_id,
+                    '/'].join(''), function(data){
+                    $('#image-list').html(data);
+                    init_cropping();    
+                
+                })
+                
+            } else if (data.status == 'failure') {
+                
+            } else {
+                // Invalid Status
+            }
+        }
+
+    });
+
 });
