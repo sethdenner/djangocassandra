db3ad4d20bac8141dd29de290fe7af76e5df2a61
Seth Denner <seth.adam.denner@gmail.com>
2012-11-18 22:40:57 -0800

    [complete #39561581] Load More Button
    Added load more button to continute loading businesses on scroll.

--- web/knotis/static/scripts/views/core.index.js
+++ web/knotis/static/scripts/views/core.index.js
@@ -5,10 +5,58 @@ $(function(){
             'items_per_row': 3
         });
 
-        $(document).load_offers(
-            'load_scroll', 
-            '.content-home'
-        );
+        var load_count, load_max = 4;
+        var load_scroll = function() {
+            load_count = 0;
+            $(document).load_offers(
+                'load_scroll', 
+                '.content-home',
+                function(
+                    data,
+                    jqxhr,
+                    status
+                ) {
+                    ++load_count;
+                    if (load_count >= load_max) {
+                        $(document).load_offers(
+                            'stop_scroll'
+                        );
+
+                    }
+                }
+            );
+        };
+        load_scroll();
+        
+        $('.load_more_results').click(function(evt){
+            if (load_count >= load_max){
+                $content = $('.content-home'); 
+                $content.load_offers(
+                    'load',
+                    function(
+                        data,
+                        jqxhr,
+                        status
+                    ) {
+                        $content.append(data);
+                        load_scroll();               
+                        
+                    },
+                    $content.attr('data-href'),
+                    $content.attr('data-business'),
+                    $content.attr('data-city'),
+                    $content.attr('data-neighboorhood'),
+                    $content.attr('data-category'),
+                    $content.attr('data-premium'),
+                    (parseInt($content.attr('data-page')) + 1).toString(),
+                    $content.attr('data-query')
+                )
+
+            }
+            evt.stopPropagation();
+            return false;
+            
+        });
         
     })();
 
