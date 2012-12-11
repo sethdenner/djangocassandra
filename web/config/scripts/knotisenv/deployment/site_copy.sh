25ce2a879cc0f3909d2c8e003a05b84147453691
Seth Denner <seth.adam.denner@gmail.com>
2012-12-11 13:06:53 -0800

    [#40991973] Automated Deployment Scripts
    Deployment script now archives previous version of the site.

--- web/config/scripts/knotisenv/deployment/site_copy.sh
+++ web/config/scripts/knotisenv/deployment/site_copy.sh
@@ -5,6 +5,13 @@
 #                               #
 #===============================#
 
-rm -rf /srv/knotis/app/prev 2> /dev/null
-mv -f /srv/knotis/app/web /srv/knotis/app/prev
+# Backup old version of the site
+if [[ -d /srv/knotis/app/web ]] ; then
+    mkdir -p /srv/knotis/archive/ 2> /dev/null
+    tar -czf /srv/knotis/archive/knotis_"$(date +%Y_%m_%d_%H%M%S)".tar.gz /srv/knotis/app/web
+    rm -rf /srv/knotis/app/prev 2> /dev/null
+    mv -f /srv/knotis/app/web /srv/knotis/app/prev
+
+fi
+
 cp ${1} /srv/knotis/app/web
--- web/config/scripts/knotisenv/deployment/start_deployment.sh
+++ web/config/scripts/knotisenv/deployment/start_deployment.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ ! -d ${1} ]] ; then
+    echo "ERROR: Directory ${1} does not exist."
+    return 1
+
+fi
+
 ./maintenance_mode enable
-./site_copy.sh $(./download_knotis.sh)
+./site_copy.sh ${1}
 ./site_refresh.sh
 ./maintenance_mode disable
