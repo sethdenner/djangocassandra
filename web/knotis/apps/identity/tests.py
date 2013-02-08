17b896f9072ff753f247ed5b97f76d3f984d3606
Seth Denner <seth.adam.denner@gmail.com>
2013-02-07 18:21:00 -0800

    [#43978741] - Identity Model
    Added basic unit test for identity model.

---
+++ web/knotis/apps/identity/tests.py
@@ -0,0 +1,30 @@
+from django.utils import unittest
+from knotis.apps.identity.models import (
+    Identity,
+    IdentityType
+)
+
+
+class IdentityTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_identity_creation(self):
+        identity_type = IdentityType.INDIVIDUAL
+        name = 'Test Identity'
+        description = 'Test Description'
+
+        created = Identity.objects.create(
+            identity_type=identity_type,
+            name=name,
+            description=description,
+            primary_image=None
+        )
+
+        selected = Identity.objects.get(pk=created.id)
+        self.assertEqual(selected.name, name)
+        self.assertEqual(selected.description, description)
+        self.assertEqual(selected.identity_type, identity_type)
+
+    def test_get_identity_images(self):
+        self.assertTrue(False)
