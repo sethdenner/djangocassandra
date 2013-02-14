99ef843f6e5f52808dc3799fa59f5829f4a2568a
Seth Denner <seth.adam.denner@gmail.com>
2013-02-14 14:06:16 -0800

    [#43978741] - Identity Model
    Identity Work (BROKEN! AMMEND ME)

--- web/knotis/contrib/identity/models.py
+++ web/knotis/contrib/identity/models.py
@@ -1,18 +1,21 @@
 from django.utils.translation import ugettext_lazy as _
-#from django.db import models
 
 from knotis.contrib.quick.models import QuickModel
 from knotis.contrib.quick.fields import (
     QuickCharField,
-    QuickDateTimeField,
-    QuickFloatField,
     QuickTextField,
     QuickIntegerField,
     QuickImageField,
 )
 
-__models__ = ( 'Identity', 'IdentityIndividual', 'IdentityBusiness', 'IdentityEstablishment')
+__models__ = (
+    'Identity',
+    'IdentityIndividual',
+    'IdentityBusiness',
+    'IdentityEstablishment'
+)
 __all__ = __models__ + ('IdentityTypes',)
+
 
 class IdentityTypes:
     UNDEFINED = -1
@@ -27,22 +30,28 @@ class IdentityTypes:
         (ESTABLISHMENT, 'Establishment'),
     )
 
+
 class Identity(QuickModel):
     identity_type = QuickIntegerField(
         choices=IdentityTypes.CHOICES,
-        default=IdentityTypes.UNDEFINED,
-        #null=True,blank=True,
+        default=IdentityTypes.UNDEFINED
     )
 
-    #user = ForeignKey(User)
-    print "Fix names to be over ridable by proxies"
-    name = QuickCharField(max_length= 80, db_index=True, verbose_name=_("Identity Name"), required=True)
-    description = QuickTextField(verbose_name=_("Describe the Identity"), required=True)
-    image = QuickImageField(default="http://placehold.it/200x200.jpg&text=N/A")
-    from django.contrib.auth.models import User
-    #user = QuickForeignKey(User)
-    
-    class Quick(QuickModel.Quick): 
+    name = QuickCharField(
+        max_length=80,
+        db_index=True,
+        verbose_name=_("Identity Name"),
+        required=True
+    )
+    description = QuickTextField(
+        verbose_name=_("Describe the Identity"),
+        required=True
+    )
+    primary_image = QuickImageField(
+        default="http://placehold.it/200x200.jpg&text=N/A"
+    )
+
+    class Quick(QuickModel.Quick):
         exclude = ()
         pass
 
@@ -51,40 +60,44 @@ class Identity(QuickModel):
             return str(self.name)
         return str(self.id)
 
+
 class IdentityIndividual(Identity):
-    class Quick(Identity.Quick): 
+    class Quick(Identity.Quick):
         exclude = ('identity_type',)
         filters = {'identity_type': IdentityTypes.INDIVIDUAL}
         name = 'individual'
 
     class Meta:
         proxy = True
+        field_overrides = {'name': {'verbose_name': _('User Name')}}
 
-    def clean(self): 
+    def clean(self):
         print ("Cleaning IdentityIndividual")
         self.identity_type = IdentityTypes.INDIVIDUAL
 
+
 class IdentityBusiness(Identity):
-    class Quick(Identity.Quick): 
+    class Quick(Identity.Quick):
         exclude = ('identity_type',)
         filters = {'identity_type': IdentityTypes.BUSINESS}
         name = 'business'
 
     views = Identity.Quick.views
-    views.update({ 
+    views.update({
         'detail_customized': 'identitybusiness/DetailView.html',
-        'detail': 'identitybusiness/DetailView.html' 
-        })
+        'detail': 'identitybusiness/DetailView.html'
+    })
 
     class Meta:
         proxy = True
 
-    def clean(self): 
+    def clean(self):
         print ("Cleaning IdentityBusiness")
         self.identity_type = IdentityTypes.BUSINESS
 
+
 class IdentityEstablishment(Identity):
-    class Quick(Identity.Quick): 
+    class Quick(Identity.Quick):
         exclude = ('identity_type',)
         filters = {'identity_type': IdentityTypes.ESTABLISHMENT}
         name = 'establishment'
@@ -92,7 +105,6 @@ class IdentityEstablishment(Identity):
     class Meta:
         proxy = True
 
-    def clean(self): 
+    def clean(self):
         print ("Cleaning IdentityEstablishment")
         self.identity_type = IdentityTypes.ESTABLISHMENT
-
--- web/knotis/contrib/quick/fields.py
+++ web/knotis/contrib/quick/fields.py
@@ -1,5 +1,6 @@
 import django.db.models as models
 
+
 class QuickFieldDefaultMixin(object):
     def __init__(
         self,
@@ -8,10 +9,11 @@ class QuickFieldDefaultMixin(object):
     ):
         if 'default' not in kwargs:
             kwargs['default'] = None
-    
-        super(QuickFieldDefaultMixin, self).__init__( *args, **kwargs)
 
-class QuickField(object):
+        super(QuickFieldDefaultMixin, self).__init__(*args, **kwargs)
+
+
+class QuickField(models.Field):
     def __init__(
         self,
         *args,
@@ -35,41 +37,77 @@ class QuickField(object):
         if 'blank' not in kwargs:
             kwargs['blank'] = True
 
-        super(QuickField, self).__init__( *args, **kwargs)
+        super(QuickField, self).__init__(*args, **kwargs)
 
     def clean(self, *args, **kwargs):
-        #print "field cleaning" + str(self.__class__)
-        return super(QuickField,self).clean(*args, **kwargs)
-    
+        return super(QuickField, self).clean(*args, **kwargs)
+
     def validate(self, *args, **kwargs):
-        #print "field validateing" + str(self.__class__)
-        return super(QuickField,self).validate(*args, **kwargs)
+        return super(QuickField, self).validate(*args, **kwargs)
+
+    @property
+    def verbose_name(self):
+        field_extras = self.field_extras()
+        if hasattr(field_extras, 'verbose_name'):
+            return field_extras['verbose_name']
+
+        return super(QuickField, self).verbose_name
+
+    @verbose_name.setter
+    def verbose_name(self, value):
+        field_extras = self.field_extras()
+        if field_extras:
+            self.Quick.field_extras['verbose_name'] = value
+
+        else:
+            super(QuickField, self).verbose_name = value
+
+    def field_extras(self):
+        if (
+            hasattr(self, 'model') and
+            hasattr(self.model, 'Quick') and
+            hasattr(self.model.Quick, 'field_extras')
+        ):
+            return self.Quick.field_extras.get(self.name, {})
+
+        else:
+            return None
+
 
 class QuickBooleanField(QuickField, models.BooleanField):
-    def __init__( self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         if 'null' not in kwargs:
             kwargs['null'] = False
         if 'default' not in kwargs:
             kwargs['default'] = False
-        super(QuickBooleanField, self).__init__( *args, **kwargs)
+        super(QuickBooleanField, self).__init__(*args, **kwargs)
+
 
 class QuickCharField(QuickField, QuickFieldDefaultMixin, models.CharField):
     pass
 
-class QuickDateTimeField(QuickField, QuickFieldDefaultMixin, models.DateTimeField):
+
+class QuickDateTimeField(
+    QuickField,
+    QuickFieldDefaultMixin,
+    models.DateTimeField
+):
     pass
+
 
 class QuickFileField(QuickField, QuickFieldDefaultMixin, models.FileField):
     pass
 
+
 class QuickFloatField(QuickField, QuickFieldDefaultMixin, models.FloatField):
     pass
 
+
 class QuickImageField(QuickField, QuickFieldDefaultMixin, models.ImageField):
-    def __init__( self, **kwargs):
+    def __init__(self, **kwargs):
         if 'upload_to' not in kwargs:
             kwargs['upload_to'] = 'image_uploads/%Y/%m/%d'
-        super(type(self), self).__init__( **kwargs)
+        super(type(self), self).__init__(**kwargs)
 
     def formfield(self, **kwargs):
         # This is a fairly standard way to set up some defaults
@@ -79,11 +117,18 @@ class QuickImageField(QuickField, QuickFieldDefaultMixin, models.ImageField):
         defaults.update(kwargs)
         return super(QuickImageField, self).formfield(**defaults)
 
-class QuickIntegerField(QuickField, QuickFieldDefaultMixin, models.IntegerField):
+
+class QuickIntegerField(
+    QuickField,
+    QuickFieldDefaultMixin,
+    models.IntegerField
+):
     pass
+
 
 class QuickTextField(QuickField, QuickFieldDefaultMixin, models.TextField):
     pass
+
 
 class QuickForeignKey(QuickField, QuickFieldDefaultMixin, models.ForeignKey):
     def __init__(
@@ -115,11 +160,14 @@ class QuickForeignKey(QuickField, QuickFieldDefaultMixin, models.ForeignKey):
         return 'ForeignKeyNonRel'
 
 from django.contrib.contenttypes import generic
+
+
 class QuickGenericForeignKey(QuickField, generic.GenericForeignKey):
-    def __init__( self, *args, **kwargs):
-        super(QuickField, self).__init__( *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(QuickField, self).__init__(*args, **kwargs)
 
 from django_extensions.db.fields import UUIDField
+
+
 class QuickUUIDField(QuickField, QuickFieldDefaultMixin, UUIDField):
     pass
-
