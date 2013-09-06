5de30c465090a7a2aab9930248e7403d841f5e0b
Seth Denner <seth@knotis.com>
2013-09-06 16:59:52 -0700

    [#56675346] - Improve Install Scripts
    Reenabling all installers (Acciedently commited them disabled while testing)

--- setup/environments/next.sh
+++ setup/environments/next.sh
@@ -9,21 +9,21 @@ export DEFAULT_INSTALL_LOCATION=/srv/knotis
 export ADMIN_USER=knotis
 export ADMIN_GROUP=knotis
 export INSTALLERS=(
-#    install_dependencies.sh
-#    install_virtualenv.sh
-#    install_java.sh
-#    install_cassandra.sh
-#    install_apache.sh
-#    install_thrift.sh
-#    install_httplib2.sh
-#    install_oauth2.sh
-#    install_mysql_python.sh
-#    install_numpy.sh
-#    install_django_nonrel.sh
-#    install_djangotoolbox.sh
-#    install_django_permission_backend_nonrel.sh
-#    install_django_dbindexer.sh
-#    install_django_extensions.sh
+    install_dependencies.sh
+    install_virtualenv.sh
+    install_java.sh
+    install_cassandra.sh
+    install_apache.sh
+    install_thrift.sh
+    install_httplib2.sh
+    install_oauth2.sh
+    install_mysql_python.sh
+    install_numpy.sh
+    install_django_nonrel.sh
+    install_djangotoolbox.sh
+    install_django_permission_backend_nonrel.sh
+    install_django_dbindexer.sh
+    install_django_extensions.sh
     install_django_cassandra.sh
     install_django_autoload.sh
     install_crispy_forms.sh
