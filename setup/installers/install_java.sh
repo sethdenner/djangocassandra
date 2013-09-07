d64ee1704c9bafa8627a48666e56dfd95c7e1924
Seth Denner <seth@knotis.com>
2013-09-06 17:15:10 -0700

    [#56675346] - Improve Install Scripts
    Make Java install without lisence prompt.

--- setup/installers/install_java.sh
+++ setup/installers/install_java.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-apt-get install -y jsvc libcap2 libcommons-daemon-java libjna-java || { echo 'Failed to install java dependencies.' >&2; exit 1; }
-
 add-apt-repository -y ppa:webupd8team/java || { echo 'Failed to add repository ppa:webupd8team/java' >&2; exit 1; }
 apt-get update
+
+apt-get install -y jsvc libcap2 libcommons-daemon-java libjna-java || { echo 'Failed to install java dependencies.' >&2; exit 1; }
+
+
+# Make java install silently. 
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+
 apt-get install -y oracle-java7-installer
