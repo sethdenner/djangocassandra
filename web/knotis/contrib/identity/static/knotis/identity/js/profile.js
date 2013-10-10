716bbcf3e49f223466210dfb82afad87a50cfd91
Seth Denner <seth.adam.denner@gmail.com>
2013-10-10 16:40:23 -0700

    Fixing badge image upload on business profile page.

--- web/knotis/contrib/identity/static/knotis/identity/js/profile.js
+++ web/knotis/contrib/identity/static/knotis/identity/js/profile.js
@@ -3,21 +3,14 @@
     $('div#id-profile-cover img[data-add-image]').click(function(event) {
         event.preventDefault();
 
-        $('#modal-box').modal({
-            backdrop: 'static'
-        });
-
         var identity_id = $('div#id-identity-id').attr('data-identity-id')
 
-        $.get(
-            '/image/upload/',
-            {},
-            function(
-                data,
-                status,
-                jqxhr
-            ) {
-                $('#modal-box').html(data);
+        $.ajaxmodal({
+            href: '/image/upload/',
+            modal_settings: {
+                backdrop: 'static'
+            },
+            on_open: function(data, status, request) {
                 $('#file-uploader').sickle({
                     do_upload: true,
                     params: {
@@ -36,10 +29,8 @@
                     related_object_id: identity_id
 
                 });
-
             }
-
-        );
+        })
 
     });
 
