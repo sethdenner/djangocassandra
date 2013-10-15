a55dc919193790674a6e25a262d93e338f58d02b
Joshua Moore <moroe3@gmail.com>
2013-10-15 15:29:19 -0700

    Edit Address works again

--- web/knotis/contrib/identity/static/knotis/identity/js/update_profile.js
+++ web/knotis/contrib/identity/static/knotis/identity/js/update_profile.js
@@ -5,49 +5,41 @@
 
     var $updateable_addresses = $('.update-address-link');
 
-    if($updateable_addresses){
-
-	var update_address = function(){
-	    var $this = $(this);
-	    $.get(
-		'/location_form/',
-		{},
-		function(data, status, jqxhr){
-		    // fill the modal
-		    $('#modal-box').modal({
-			backdrop: 'static',
-			keyboard: false,
-		    });
-		    
-		    $('#modal-box').html(data);
-
-		    // submit form and close modal, when done
-		    $('#id-location-form').ajaxform({
-			done: function(data, status, jqxhr){
-			    if(!data.errors){
-				$this.text($('#id_address').val());
-				$('#modal-box').modal('hide');
-			    }else{
-				console.log(data.errors);
-			    }
-			},
-			method: 'post'
-		    });
-
-		    $('#modal-box').modal();
-		    
-		    $('#id-location-form #address-input #id_address').geocomplete({
-			map: '.map_canvas',
-			location: $this.text(),
-			details: '#id-location-form',
-			detailsAttribute: 'data-geo'
-		    });
-
+    var update_address = function(){
+	var $this = $(this);
+	$.ajaxmodal({
+	    href: '/location_form/',
+	    modal_settings: {
+		backdrop: 'static',
+		keyboard: true
+	    },
+	    on_open: function(data, status, request){
+		$('#id-location-form').ajaxform({
+		    done: function(data, status, jqxhr){
+			if(!data.errors){
+			    $this.text($('#id_address').val());
+			    $('#modal-box').modal('hide');
+			}
+		    }
 		});
-	};
 
-	$updateable_addresses.on('click', update_address);
-    }
+		$('#id-location-form #address-input #id_address').geocomplete({
+		    map: '.map_canvas',
+		    location: $this.text(),
+		    details: '#id-location-form',
+		    detailsAttribute: 'data-geo'
+		});
+
+		$('form#id-location-form input#related-id-input').val(
+		    $('#id-identity-id').attr('data-identity-id')
+		);
+	    }
+	});
+
+    };
+
+
+    $updateable_addresses.on('click', update_address);
 
 
     // ENDPOINT EDITING
