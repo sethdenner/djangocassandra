fa1f1da9e4dbee206ad3e97b4eeb0dd64f66f89e
Joshua Moore <moroe3@gmail.com>
2013-10-21 15:02:51 -0700

    [Finish #59285886] Edit Establishment Badge Image After Creation

--- web/knotis/contrib/identity/static/knotis/identity/js/update_profile.js
+++ web/knotis/contrib/identity/static/knotis/identity/js/update_profile.js
@@ -168,5 +168,35 @@
 	    }
 	});
     });
+
+
+    $('a.change-profile-badge-link').click(function(event){
+	event.preventDefault();
+	var identity_id = $('div#id-identity-id').attr('data-identity-id');
+
+	$.ajaxmodal({
+	    href: '/image/upload',
+	    modal_settings: {
+		backdrop: 'static'
+	    },
+	    on_open: function(data, status, request){
+
+		$('#file-uploader').sickle({
+		    do_upload: true,
+		    params: {
+			type: 'image'
+		    },
+		    aspect: 1,
+		    related_object_id: identity_id,
+		    context: 'profile_badge',
+		    done: function(data){
+			$('modal-box').modal('hide');
+		    }
+		
+		});		
+	    }
+	});
+    });
+
     
 })(jQuery);
