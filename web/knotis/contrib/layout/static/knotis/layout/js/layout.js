92f8b3f9e04ec728608cb975c7f868733afc5428
Seth Denner <seth.adam.denner@gmail.com>
2013-12-03 16:26:07 -0800

    [#61856740] - Updated Signup View
    Updated CSS and Markup for signup view.

--- web/knotis/contrib/layout/static/knotis/layout/js/layout.js
+++ web/knotis/contrib/layout/static/knotis/layout/js/layout.js
@@ -2,8 +2,18 @@
 
     $('.modal-link').click(function(event) {
 	    event.preventDefault();
+
+        var modal_width = $(this).attr('data-modal-width');
+        var modal_id = $(this).attr('id');
+        if (modal_id) {
+            modal_id = modal_id + '-modal';
+        } else {
+            modal_id = 'modal-box';
+        }
         $.ajaxmodal({
-            href: $(this).attr('href')
+            href: $(this).attr('href'),
+            modal_width: modal_width,
+            modal_id: modal_id
         });
     });
 
