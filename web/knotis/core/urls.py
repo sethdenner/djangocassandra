826fc5a96a0a3707c0897e79fb69cd146f57260f
Seth Denner <seth.adam.denner@gmail.com>
2014-01-10 14:58:56 -0800

    [Finish #61863608] - Terms and Conditions View
    Created Terms and Conditions View. Hooked it up to /terms-and-conditions/

--- web/knotis/core/urls.py
+++ web/knotis/core/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls.defaults import patterns, url
 
-from views import IndexView
+from views import (
+    IndexView,
+    TermsAndConditionsView
+)
 
 urlpatterns = patterns(
     '',
     url(
         r'^[/]?$',
         IndexView.as_view()
+    ),
+    url(
+        r'^terms-and-conditions/$',
+        TermsAndConditionsView.as_view()
     ),
 )
--- web/knotis/core/views.py
+++ web/knotis/core/views.py
@@ -10,6 +10,45 @@ from knotis.contrib.maps.views import GoogleMap
 from knotis.views import ContextView
 
 
+class TermsAndConditionsView(ContextView):
+    template_name = 'knotis/terms.html'
+
+    def process_context(self):
+        styles = self.context.get('styles', [])
+        post_scripts = self.context.get('post_scripts', [])
+
+        my_styles = [
+            'knotis/layout/css/global.css',
+            'knotis/layout/css/header.css',
+            'navigation/css/nav_top.css',
+            'navigation/css/nav_side.css',
+        ]
+
+        for style in my_styles:
+            if not style in styles:
+                styles.append(style)
+
+        my_post_scripts = [
+            'knotis/layout/js/layout.js',
+            'knotis/layout/js/forms.js',
+            'knotis/layout/js/header.js',
+            'navigation/js/navigation.js',
+            'knotis/api/js/api.js',
+        ]
+
+        for script in my_post_scripts:
+            if not script in post_scripts:
+                post_scripts.append(script)
+
+        local_context = copy.copy(self.context)
+        local_context.update({
+            'styles': styles,
+            'post_scripts': post_scripts,
+            'fixed_side_nav': True
+        })
+        return local_context
+
+
 class IndexView(ContextView):
     template_name = 'knotis/layout/index.html'
 
