dd47fc0baabcd39919f2e1fe8244b17d3894ad09
Seth Denner <seth@knotis.com>
2014-03-03 05:09:00 -0800

    ok this time it's right for real

--- web/config/stage.py
+++ web/config/stage.py
@@ -1,6 +1,12 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django_cassandra.db', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.                                                           'NAME': 'knotis', # Or path to database file if using sqlite3.                     'USER': '', # Not used with sqlite3.                                               'PASSWORD': '', # Not used with sqlite3.                                           'HOST': '', # Set to empty string for localhost. Not used with sqlite3.            'PORT': '', # Set to empty string for default. Not used with sqlite3.          }
+        'ENGINE': 'django_cassandra.db', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'knotis', # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 HAYSTACK_CONNECTIONS = {
