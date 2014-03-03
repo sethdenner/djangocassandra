68c258c5dfabee2a747be3ba1a61a5ecd9801142
Seth Denner <seth@knotis.com>
2014-03-03 05:05:57 -0800

    Added missing variable to stage config

--- web/config/stage.py
+++ web/config/stage.py
@@ -1,12 +1,6 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django_cassandra.db', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.                                                     
-        'NAME': 'knotis', # Or path to database file if using sqlite3.                                                                                                  
-        'USER': '', # Not used with sqlite3.                                                                                                                            
-        'PASSWORD': '', # Not used with sqlite3.                                                                                                                        
-        'HOST': '74.121.189.162', # Set to empty string for localhost. Not used with sqlite3.                                                                           
-        'PORT': '5551', # Set to empty string for default. Not used with sqlite3.                                                                                       
-    }
+        'ENGINE': 'django_cassandra.db', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.                                                           'NAME': 'knotis', # Or path to database file if using sqlite3.                     'USER': '', # Not used with sqlite3.                                               'PASSWORD': '', # Not used with sqlite3.                                           'HOST': '', # Set to empty string for localhost. Not used with sqlite3.            'PORT': '', # Set to empty string for default. Not used with sqlite3.          }
 }
 
 HAYSTACK_CONNECTIONS = {
@@ -16,6 +10,13 @@ HAYSTACK_CONNECTIONS = {
         'INDEX_NAME': 'haystack',
     },
 }
+
+EMAIL_BILLING_USER = 'knotis.int+billing@gmail.com'
+EMAIL_HOST_USER = 'knotis.int@gmail.com'
+EMAIL_HOST_PASSWORD = 'wheeling and dealing'
+
+# Base url for external access
+BASE_URL = 'http://stage.knotis.com' # NO TRAILING SLASH!!!
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
