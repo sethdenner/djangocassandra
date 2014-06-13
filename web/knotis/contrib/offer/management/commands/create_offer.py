bd3036dca127413d9659723921988d02dcad50af
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-06-13 11:04:00 -0700

    Refactored create_offer management command to just call the API.

--- web/knotis/contrib/offer/management/commands/create_offer.py
+++ web/knotis/contrib/offer/management/commands/create_offer.py
@@ -1,20 +1,11 @@
 from django.core.management.base import BaseCommand
 from optparse import make_option
-from knotis.contrib.identity.models import (
-    Identity,
-    IdentityTypes
-)
 
 from django.utils.log import logging
 logger = logging.getLogger(__name__)
 from datetime import datetime
 
-from knotis.contrib.product.models import Product
-from knotis.contrib.inventory.models import Inventory
-from knotis.contrib.offer.models import Offer, OfferPublish
-from knotis.contrib.media.models import ImageInstance
-from knotis.contrib.endpoint.models import Endpoint, EndpointTypes
-
+from knotis.contrib.offer.api import OfferCreateApi
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -36,7 +27,7 @@ class Command(BaseCommand):
 
         make_option('--value',
             dest='value',
-            default=10,
+            default=20,
             type=int,
             help='Value for the offer'),
 
@@ -59,6 +50,23 @@ class Command(BaseCommand):
             dest='description',
             default='',
             help='Description for the offer'),
+
+        make_option('--restrictions',
+            dest='restrictions',
+            default='',
+            help='Restrictions for the offer'),
+
+        make_option('--is_physical',
+            dest='is_physical',
+            action="store_true",
+            default=False,
+            help='If the offer is for a physical item'),
+
+        make_option('--stock',
+            dest='stock',
+            default=-1,
+            type=int,
+            help=''),
         )
 
     def handle(
@@ -66,82 +74,4 @@ class Command(BaseCommand):
         *args,
         **options
     ):
-        owner_name = options['owner']
-        try:
-            owner_identity = Identity.objects.get(
-                name=owner_name,
-                identity_type=IdentityTypes.BUSINESS
-            )
-        except:
-            logger.exception('Cannot find owner %s' % owner_name)
-            raise
-
-
-        currency_name = options['currency']
-        currency = Product.currency.get(currency_name)
-
-        price = options['price']
-        value = options['value']
-        product = Product.objects.get_or_create_credit(
-            price,
-            value,
-            currency.sku
-        )
-
-        print owner_name, owner_identity, product
-
-
-        inventory = Inventory.objects.create_stack_from_product(
-            owner_identity,
-            product,
-            price=value,
-            unlimited=True,
-        )
-
-        split_inventory = Inventory.objects.split(
-            inventory,
-            owner_identity,
-            1
-        )
-        title = options['title']
-
-        photos = ImageInstance.objects.filter(
-            owner=owner_identity,
-            context='offer_banner',
-        )
-
-
-        offer = Offer.objects.create(
-            owner=owner_identity,
-            title=title,
-            start_time=options['start_time'],
-            end_time=options['end_time'],
-            stock=None,
-            unlimited=True,
-            inventory=[split_inventory],
-            discount_factor=price / value
-        )
-
-        offer.save()
-
-        new_photo = ImageInstance.objects.create(
-            owner=owner_identity,
-            image=photos[0].image,
-            related_object_id=offer.id,
-            context='offer_banner',
-            primary=True
-        )
-        endpoint_current_identity = Endpoint.objects.get(
-            endpoint_type=EndpointTypes.IDENTITY,
-            identity=owner_identity
-        )
-        OfferPublish.objects.create(
-            endpoint=endpoint_current_identity,
-            subject=offer,
-            publish_now=True
-        )
-
-        offer.default_image = new_photo
-        offer.save()
-
-        print offer
+        OfferCreateApi.create_offer(**options)
