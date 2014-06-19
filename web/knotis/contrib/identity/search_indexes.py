03efcfdd8a3e2db46bfa3ff4ff03821de7330119
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-06-19 12:20:23 -0700

    Added another index to identity establishment.
    The 'available' column wasn't in the IdentityEstablishmentSearchIndex

--- web/knotis/contrib/identity/search_indexes.py
+++ web/knotis/contrib/identity/search_indexes.py
@@ -25,6 +25,7 @@ class IdentityEstablishmentIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     author = indexes.CharField(model_attr='name')
     pub_date = indexes.DateTimeField(model_attr='pub_date')
+    available = indexes.BooleanField(model_attr='available')
 
     location = indexes.LocationField(model_attr='get_location')
 
