ceee6fb7cec3a411b9a70e71b03789f63ff44a30
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-07-23 21:09:29 -0700

    Added search views.

--- web/knotis/contrib/search/views.py
+++ web/knotis/contrib/search/views.py
@@ -11,16 +11,16 @@ logger = logging.getLogger(__name__)
 
 from knotis.contrib.identity.views import IdentityTile, get_current_identity
 from knotis.contrib.offer.views import OfferTile
-from knotis.contrib.layout.views import GridSmallView
+from knotis.contrib.layout.views import GridSmallView, DefaultBaseView
 from knotis.contrib.search.api import SearchApi
-from knotis.views import FragmentView
-from knotis.contrib.identity.views import get_current_identity
-from knotis.contrib.identity.models import IdentityTypes
-
-from forms import SearchForm
+from knotis.views import EmbeddedView
 
 
-class SearchResultsView(FragmentView):
+class SearchResultsView(EmbeddedView):
+    default_parent_view_class = DefaultBaseView
+    url_patterns = [
+        r'^search/',
+    ]
     template_name = 'search/search.html'
     view_name = 'search_results'
 
@@ -33,51 +33,11 @@ class SearchResultsView(FragmentView):
         return HttpResponse('OK')
 
     def process_context(self):
-        styles = self.context.get('styles', [])
-        post_scripts = self.context.get('post_scripts', [])
+        search_query = self.context.get('q', None)
 
-        my_styles = [
-            'knotis/layout/css/global.css',
-            'knotis/layout/css/header.css',
-            'knotis/layout/css/grid.css',
-            'knotis/layout/css/tile.css',
-            'navigation/css/nav_top.css',
-            'navigation/css/nav_side.css',
-            'styles/default/fileuploader.css'
-        ]
-
-        for style in my_styles:
-            if not style in styles:
-                styles.append(style)
-
-        my_post_scripts = [
-            'knotis/layout/js/layout.js',
-            'knotis/layout/js/forms.js',
-            'knotis/layout/js/header.js',
-            'knotis/layout/js/create.js',
-            'navigation/js/navigation.js',
-            'jcrop/js/jquery.Jcrop.js',
-            'scripts/fileuploader.js',
-            'scripts/jquery.colorbox.js',
-            'scripts/jquery.sickle.js',
-            'knotis/identity/js/profile.js',
-            'knotis/api/js/api.js',
-            'knotis/search/js/searchresults.js',
-            'knotis/identity/js/business-tile.js'
-        ]
-
-        for script in my_post_scripts:
-            if not script in post_scripts:
-                post_scripts.append(script)
-
-        local_context = copy.copy(self.context)
-        local_context.update({
-            'styles': styles,
-            'post_scripts': post_scripts,
-            'fixed_side_nav': True,
+        return self.context.update({
+            'search_query': search_query,
         })
-
-        return local_context
 
 
 class SearchResultsGrid(GridSmallView):
@@ -108,7 +68,6 @@ class SearchResultsGrid(GridSmallView):
         tiles = []
 
         for result in search_results:
-            #if result.object.content_type == ContentType.objects.get('identity establishment'):
             try:
                 if result.content_type.name == 'identity establishment':
                     business_tile = IdentityTile()
@@ -150,33 +109,6 @@ class SearchResultsGrid(GridSmallView):
             'tiles': tiles,
             'search_results': search_results,
             'query': query
-        })
-
-        return local_context
-
-
-class SearchBarView(FragmentView):
-    template_name = 'search/search_bar.html'
-    view_name = 'search_bar'
-
-    def post(
-        self,
-        request,
-        *args,
-        **kwargs
-    ):
-        return HttpResponse('OK')
-
-    def process_context(self):
-        form_args = {}
-        search_query = self.request.GET.get('q',None)
-        if search_query:
-            form_args['q'] = search_query
-
-        local_context = copy.copy(self.context)
-        local_context.update({
-            'search_form': SearchForm(form_args),
-            'search_query': search_query
         })
 
         return local_context
