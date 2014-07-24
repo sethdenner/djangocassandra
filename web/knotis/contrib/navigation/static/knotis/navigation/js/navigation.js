47623d9f757e341ff0e894004b41b3377a8562d6
Seth Denner <seth.adam.denner@gmail.com>
2014-07-23 18:53:40 -0700

    Navigation Fixes
    Fixed some issues with back/forward buttons.

--- web/knotis/contrib/navigation/static/knotis/navigation/js/navigation.js
+++ web/knotis/contrib/navigation/static/knotis/navigation/js/navigation.js
@@ -3,11 +3,16 @@
 (function($) {
     "use strict";
 
-    var navigation_stack = [];
-    var current_navigation = null;
-    var next_navigation = null;
+    var history = [];
+    var history_index = null;
+    var current_history = null;
+    var next_history = null;
 
     var first_change = true;
+
+    var modal_onclose = function () {
+
+    };
 
     var initialize_once = function () {
         $('#accordion-nav > li[data-target]').hover(
@@ -23,22 +28,34 @@
             }
         );
 
-        next_navigation = {
+        $('div.modal').each(function() {
+            var $modal = $(this);
+            $modal.modal();
+            $modal.on('hidden.bs.modal', modal_onclose);
+
+        });
+
+        var $default_anchor = $('<a></a>');
+        $default_anchor.attr('href', window.location.pathname);
+        $default_anchor.attr('data-target-id', 'main-content');
+        $default_anchor.addClass('defaultAnchor');
+
+        next_history = {
             address: window.location.pathname,
-            anchor: null
+            anchor: $default_anchor
         }
 
     };
 
     var initialize_address = function (force) {
-        $('a:not([data-dismiss-modal])').off('click.address').on(
+        $('a:not(.no-deep)').off('click.address').on(
             'click.address',
             function (event) {
                 event.preventDefault();
 
                 var $anchor = $(this);
                 var href = $anchor.attr('href');
-                next_navigation = {
+                next_history = {
                     address: href,
                     anchor: $anchor
                 };
@@ -65,27 +82,6 @@
             }
         });
 
-        $('a[data-dismiss-modal]').each(function() {
-            $(this).off('click.dismiss').on('click.dismiss', function (event) {
-                event.preventDefault();
-                var $this = $(this);
-                var navigation_item = navigation_stack.pop();
-                var dismiss_modal = $this.attr('data-dismiss-modal');
-                var autoUpdate = $.address.autoUpdate();
-                var histroy = $.address.history();
-                $.address.autoUpdate(false);
-                $.address.history(false);
-                $.address.value(navigation_item.address);
-                next_navigation = 'noop';
-                $.address.update();
-                $.address.history(history);
-                $.address.autoUpdate(autoUpdate);
-                $('#' + dismiss_modal).modal('hide');
-
-            });
-
-        });
-
         initialize_address();
     };
 
@@ -94,8 +90,8 @@
         initialize_always();
 
         $.address.state('/').change(function (event) {
-            if (next_navigation == 'noop') {
-                next_navigation = null;
+            if (next_history == 'noop') {
+                next_history = null;
                 return;
 
             }
@@ -106,16 +102,36 @@
                 // The change event gets triggered in the inital
                 // page request. Since the page is already
                 // rendered no action is necessary here.
-                current_navigation = next_navigation;
-                next_navigation = null;
+                current_history = next_history;
+                next_history = null;
                 first_change = false;
                 return;
 
             }
 
-            if (!next_navigation) {
-                if (navigation_stack.length) {
-                    next_navigation = navigation_stack.pop();
+            var back = false;
+            var forward = false;
+            if (!next_history) {
+                if (history.length) {
+                    if (
+                        history_index >= 1 &&
+                        history[history_index - 1].address === address
+                    ) {
+                        back = true;
+                        --history_index;
+
+                    } else if (
+                        history_index < history.length - 1 &&
+                        history[history_index + 1].address === address
+                    ) {
+                        forward = true;
+                        ++history_index;
+
+                    } else {
+                        alert('There was a navigation error :(');
+
+                    }
+                    next_history = history[history_index];
 
                 } else {
                     return;
@@ -124,79 +140,85 @@
 
             }
 
-            var $next_anchor = next_navigation.anchor;
-            
-            if ($next_anchor.hasClass('modal-link')) {
-                var modal_width = $next_anchor.attr('data-modal-width');
-                var new_modal_id = $next_anchor.attr('data-modal-id');
-                if (modal_id != new_modal_id) {
-                    $('#' + modal_id).modal('hide');
-                }
-                var modal_id = new_modal_id;
+            var current_is_modal = current_history.anchor.hasClass('modal-link');
+            var next_is_modal = next_history.anchor.hasClass('modal-link');
+            if (current_is_modal && !next_is_modal) {
+                $('div.modal').modal('hide');
 
-                if (!modal_id) {
-                    modal_id = 'modal-box';
-
-                }
-                $.ajaxmodal({
-                    href: $next_anchor.attr('href'),
-                    modal_width: modal_width,
-                    modal_id: modal_id,
-                    on_open: function (data, status, request) {
-                        initialize_always();
-
-                    }
-                });
-
-            } else {
-                // Hide all modals before rendering this view.
-                var target_element_id = $next_anchor.attr('data-target-id');
-                if (typeof target_element_id === typeof undefined || target_element_id === false) {
-                    alert('Anchor element has no "data-target-id" attribute defined!');
-                    return;
-                }
-
-                var $target_element = $('#' + target_element_id);
-                if (!$target_element.length) {
-                    alert('Target element #' + target_element_id + 'does not exist in page.');
-                    return;
-                }
-
-                $.ajax({
-                    url: address,
-                    data: 'format=json',
-                    complete: function (request, status) {
-                        var data = $.parseJSON(request.responseText);
-                        if (data.errors || status === 'error') {
-                            alert([
-                                'There was an error processing your request.',
-                                '\n    status: ',
-                                status,,
-                                '\n    response: ',
-                                request.responseText
-                            ].join(''));
-                            return;
-                        }
-
-                        $('div.modal').modal('hide');
-                        if (data.title) {
-                            $.address.title(data.title);
-                            
-                        }
-                        $target_element.html(data.html);
-                        initialize_always();
-
-                    }
-                });
             }
 
+            var $next_anchor = next_history.anchor;
+
+            var dismiss_modal = false;
             var dismiss_modal_attr = $next_anchor.attr('data-dismiss-modal');
-            if (typeof dismiss_modal_attr === typeof undefined || dismiss_modal_attr === false) {
-                navigation_stack.push(current_navigation);
-                current_navigation = next_navigation;
+            if (typeof undefined !== typeof dismiss_modal_attr && false !== dismiss_modal_attr) {
+                dismiss_modal = true;
 
             }
-            next_navigation = null;
+
+            if (!dismiss_modal) {
+                if ($next_anchor.hasClass('modal-link')) {
+                    var modal_width = $next_anchor.attr('data-modal-width');
+                    $.ajaxmodal({
+                        href: $next_anchor.attr('href'),
+                        modal_width: modal_width,
+                        on_open: function (data, status, request) {
+                            initialize_always();
+
+                        },
+                        on_close: modal_onclose
+                    });
+
+                } else {
+                    // Hide all modals before rendering this view.
+                    var target_element_id = $next_anchor.attr('data-target-id');
+                    if (typeof target_element_id === typeof undefined || target_element_id === false) {
+                        alert('Anchor element has no "data-target-id" attribute defined!');
+                        return;
+                    }
+
+                    var $target_element = $('#' + target_element_id);
+                    if (!$target_element.length) {
+                        alert('Target element #' + target_element_id + 'does not exist in page.');
+                        return;
+                    }
+
+                    $.ajax({
+                        url: address,
+                        data: 'format=json',
+                        complete: function (request, status) {
+                            var data = $.parseJSON(request.responseText);
+                            if (data.errors || status === 'error') {
+                                alert([
+                                    'There was an error processing your request.',
+                                    '\n    status: ',
+                                    status,,
+                                    '\n    response: ',
+                                    request.responseText
+                                ].join(''));
+                                return;
+                            }
+
+                            $('div.modal').modal('hide');
+                            if (data.title) {
+                                $.address.title(data.title);
+                                
+                            }
+                            $target_element.html(data.html);
+                            initialize_always();
+
+                        }
+                    });
+                }
+            }
+
+            if (!forward && !back) {
+                history.push(current_history);
+                history_index = history.length;
+
+            }
+            current_history = next_history;
+            next_history = null;
 
         });
 
