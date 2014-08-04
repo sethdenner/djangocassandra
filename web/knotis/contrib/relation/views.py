295579d7c42356d9773ae1498d562af625870238
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-08-04 16:51:52 -0700

    Fix #25 and #26.
    Unfollow businesses in my following view.
    
    Businesses and Superusers now no longer have the ability to follow
    anyone. Only Individuals.

--- web/knotis/contrib/relation/views.py
+++ web/knotis/contrib/relation/views.py
@@ -45,31 +45,6 @@ class NewPermissionEmailBody(EmailView):
         return local_context
 
 
-class NewFollowerEmailBody(EmailView):
-    template_name = 'knotis/relation/email_new_follower.html'
-
-    def process_context(self):
-        local_context = copy.copy(self.context)
-
-        browser_link = 'example.com'
-        business_name = 'Fine Bitstrings'
-        business_cover_url = '/media/cache/ef/25/ef2517885c028d7545f13f79e5b7993a.jpg'
-        business_logo_url = '/media/cache/87/08/87087ae77f4a298e550fc9d255513ad4.jpg'
-        username = '@username'
-        view_profile_link = 'example.com'
-
-        local_context.update({
-            'browser_link': browser_link,
-            'business_cover_url': business_cover_url,
-            'business_logo_url': business_logo_url,
-            'business_name': business_name,
-            'username': username,
-            'view_profile_link': view_profile_link
-        })
-
-        return local_context
-
-
 class MyFollowingView(EmbeddedView):
     template_name = 'knotis/relation/follow_display_view.html'
     default_parent_view_class = DefaultBaseView
@@ -93,7 +68,6 @@ class MyFollowingView(EmbeddedView):
         )
 
         term = ''
-        related_parties = []
         if (
             IdentityTypes.INDIVIDUAL == current_identity.identity_type or
             IdentityTypes.SUPERUSER == current_identity.identity_type
@@ -103,18 +77,16 @@ class MyFollowingView(EmbeddedView):
                 relation_type=RelationTypes.FOLLOWING,
                 subject_object_id=current_identity.id
             )
-            for relation in relations:
-                related_parties.append(relation.related)
-        elif (
-            IdentityTypes.ESTABLISHMENT == current_identity.identity_type
-        ):
+            related_parties = [relation.related for relation in relations]
+
+        elif (IdentityTypes.ESTABLISHMENT == current_identity.identity_type):
             term = 'Followers'
             relations = Relation.objects.filter(
                 relation_type=RelationTypes.FOLLOWING,
                 related_object_id=current_identity.id
             )
-            for relation in relations:
-                related_parties.append(relation.subject)
+            related_parties = [relation.subject for relation in relations]
+
         else:
             related_parties = []
             term = 'Following'
