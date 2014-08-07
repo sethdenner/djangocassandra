dcf1164420eedcc8f9162f60d0da866f86ea9186
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-08-07 15:10:16 -0700

    updated location viws and urls for new modal view

--- web/knotis/contrib/location/views.py
+++ web/knotis/contrib/location/views.py
@@ -1,18 +1,11 @@
-import copy
+from knotis.contrib.layout.views import DefaultBaseView
+from knotis.views import ModalView
 
-from knotis.contrib.maps.forms import GeocompleteForm
-from knotis.views import FragmentView
 
-class LocationFormView(FragmentView):
+class LocationModal(ModalView):
     template_name = 'knotis/location/location.html'
     view_name = 'location_form_view'
-    
-    def process_context(self):
-        local_context = copy.copy(self.context)
-        local_context.update({
-            'location_form': GeocompleteForm()
-        })
-
-        return local_context
-
-
+    default_parent_view_class = DefaultBaseView
+    url_patterns = [
+        r'^location_form/$',
+    ]
