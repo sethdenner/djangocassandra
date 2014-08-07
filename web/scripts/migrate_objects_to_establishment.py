5bf1c3b468563b9771fed1f640eb4fd7b2337a82
Seth Denner <seth.adam.denner@gmail.com>
2014-08-06 17:55:21 -0700

    Work on inventory migration script

--- web/scripts/migrate_objects_to_establishment.py
+++ web/scripts/migrate_objects_to_establishment.py
@@ -1,13 +1,126 @@
 from django.utils.log import logging
 logger = logging.getLogger(__name__)
 
+from knotis.contrib.identity.models import IdentityTypes
+
+
+class InvalidMigrationSourceException(Exception):
+    pass
+
+
+def get_migration_target(business):
+    from knotis.contrib.identity.models import IdentityEstablishment
+
+    if IdentityTypes.BUSINESS != business.identity_type:
+        raise InvalidMigrationSourceException()
+
+    try:
+        establishments = (
+            IdentityEstablishment.objects.get_establishments(business)
+        )
+
+    except Exception, e:
+        logger.exception(e.message)
+        return None
+
+    if 1 == len(establishments):
+        establishment = establishments[0]
+
+    else:
+        establishment = None
+        for e in establishments:
+            if e.name == business.name:
+                establishment = e
+
+    return establishment
+
+
+def move_inventory_to_establishment():
+    from knotis.contrib.inventory.models import Inventory
+
+    all_inventory = Inventory.objects.all()
+    failed_inventory_migrations = []
+    x = 0
+    chunk_size = 20
+    inventory_chunk = all_inventory[x:x + chunk_size]
+
+    while len(inventory_chunk):
+        for i in inventory_chunk:
+            if (
+                IdentityTypes.BUSINESS != i.provider.identity_type and
+                IdentityTypes.BUSINESS != i.recipient.identity_type
+            ):
+                continue
+
+            try:
+                establishment_provider = get_migration_target(i.provider)
+
+            except InvalidMigrationSourceException:
+                pass
+
+            try:
+                establishment_recipient = get_migration_target(i.recipient)
+
+            except InvalidMigrationSourceException:
+                pass
+
+            if not establishment_provider and not establishment_recipient:
+                failed_inventory_migrations.append({
+                    'inventory': i,
+                    'to_provider': establishment_provider,
+                    'to_recipient': establishment_recipient
+                })
+                continue
+
+            original_provider = i.provider
+            original_recipient = i.recipient
+
+            if establishment_provider:
+                i.provider = establishment_provider
+
+            if establishment_recipient:
+                i.recipient = establishment_recipient
+
+            if establishment_recipient or establishment_provider:
+                try:
+                    i.save()
+
+                    logger.info(''.join([
+                        'Migration of inventory ',
+                        i.pk,
+                        ' from provider: ',
+                        original_provider.name,
+                        ' (',
+                        str(original_provider.identity_type),
+                        ') ',
+                        ' to provider: ',
+                        i.provider.name,
+                        ' (',
+                        str(i.provider.identity_type),
+                        ') ',
+                        ' and from recipient: ',
+                        original_recipient.name,
+                        ' (',
+                        str(original_recipient.identity_type),
+                        ') ',
+                        ' to recipient: ',
+                        i.recipient.name,
+                        ' (',
+                        str(i.recipient.identity_type),
+                        ') |Success!'
+                    ]))
+
+                except Exception, e:
+                    logger.exception(e.message)
+
+        x += chunk_size
+        inventory_chunk = all_inventory[x:x + chunk_size]
+
+    return failed_inventory_migrations
+
 
 def move_offers_to_establishment():
     from knotis.contrib.offer.models import Offer
-    from knotis.contrib.identity.models import (
-        IdentityEstablishment,
-        IdentityTypes
-    )
     from knotis.contrib.transaction.models import Transaction
 
     offers_migrated_total = 0
@@ -23,23 +136,7 @@ def move_offers_to_establishment():
             if IdentityTypes.BUSINESS != o.owner.identity_type:
                 continue
 
-            try:
-                establishments = (
-                    IdentityEstablishment.objects.get_establishments(o.owner)
-                )
-
-            except Exception, e:
-                logger.exception(e.message)
-                continue
-
-            if 1 == len(establishments):
-                establishment = establishments[0]
-
-            else:
-                establishment = None
-                for e in establishments:
-                    if e.name == o.owner.name:
-                        establishment = e
+            establishment = get_migration_target(o.owner)
 
             if not establishment:
                 logger.error(' '.join([
