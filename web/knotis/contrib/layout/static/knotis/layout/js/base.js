b24bf5a46c4ac3117009b361d8ad36d045e160c7
David Ortiz <david@davidortiz.me>
2014-08-11 12:25:12 -0700

    Fix to Sidebar Animation

--- web/knotis/contrib/layout/static/knotis/layout/js/base.js
+++ web/knotis/contrib/layout/static/knotis/layout/js/base.js
@@ -3,12 +3,12 @@
 
 (function ($) {
     "use strict";
-    
+
     $( "#sidebar-wrapper").hover(function () {
         $( "#sidebar-wrapper").animate({
             width: "250px",
         },  20, "linear");
-        
+
         $('.sidebar-label').fadeIn('fast');
         $('.sidebar-overlay').fadeIn('fast');
     }, function () {
@@ -18,41 +18,26 @@
         $('.sidebar-overlay').fadeOut(10);
     });
 
-    $('.sidebar-nav li').each(function (){
+    $('.sidebar-nav li').each(function () {
         $(this).hover(function (){
             $(this).css('background', '#3b3b3b');
-            $('.sidebar-label').removeClass('sidebar-label-selected');
-            $('.sidebar-label').addClass('sidebar-label-selected');
-            $(this).find('.sidebar-label-teal').addClass('sidebar-label-selected');
-            $('.sidebar-label').css('color', '#363636');
-            $(this).find('.circle').css('background', '#363636');
-            $('.sidebar-label').addClass('sidebar-label-selected');
-        }, function (){
-            $(this).css('background', '#fff');
-            $(this).find('.circle').css('background', '#595959');
+            $(this).find('.sidebar-label').css('color', '#ffffff');
+        }, function(){
+            $(this).css('background', '');
+            $(this).find('.sidebar-label').css('color', '');
         });
-    });
 
-    $('.sidebar-nav li').each(function (){
+        $(this).click(function() {
 
-        $('.sidebar-nav li').css('background', '#fff');
+            $(this).css('background', '');
+            $(this).find('.sidebar-label').css('color', '');
 
-        $(this).click(function (){
-            $('.sidebar-nav li').css('background', '#FFF');
-            $(this).addClass('sidebar-li-selected');
-            
-            $(this).each(function (){
-                $(this).css('background', '#2cc397');
-                $('.sidebar-label').addClass('sidebar-label-selected');
-                $('.sidebar-label').addClass('sidebar-label-teal');
+            $('.sidebar-nav li').each(function () {
+                $(this).removeClass('sidebar-li-selected');
+                $(this).find('.sidebar-label').removeClass('sidebar-label-selected');
             });
-
-            $( "#sidebar-wrapper" ).animate({
-                width: "70px",
-            },  20, "linear");
-
-            $('.sidebar-overlay').fadeOut(10);
-
+            $(this).addClass('sidebar-li-selected');
+            $(this).find('.sidebar-label').addClass('sidebar-label-selected');
         });
     });
 
