e1a4021c4b0e766197eb83ab7a8fa066270abd1f
Seth Denner <seth.adam.denner@gmail.com>
2014-08-11 13:18:22 -0700

    Bug 77 - css on terms-and-conditions page (from printable voucher)
    Ok I made it so these templates get rendered as the new embedded views.
    Made all the links to these pages have the attribute target="_blank" so they open in new windows/tabs
    Refactored the templates into the terms app where they belong.
    David still needs to style these pages but they are readable at the moment.

--- web/knotis/contrib/terms/views.py
+++ web/knotis/contrib/terms/views.py
@@ -1,47 +1,11 @@
 import copy
-from knotis.views import ContextView
-
-class TermsAndConditionsView(ContextView):
-    template_name = 'knotis/terms.html'
-
-    def process_context(self):
-        styles = self.context.get('styles', [])
-        post_scripts = self.context.get('post_scripts', [])
-
-        my_styles = [
-            'knotis/layout/css/global.css',
-            'knotis/layout/css/header.css',
-            'navigation/css/nav_top.css',
-            'navigation/css/nav_side.css',
-        ]
-
-        for style in my_styles:
-            if not style in styles:
-                styles.append(style)
-
-        my_post_scripts = [
-            'knotis/layout/js/layout.js',
-            'knotis/layout/js/forms.js',
-            'knotis/layout/js/header.js',
-            'navigation/js/navigation.js',
-            'knotis/api/js/api.js',
-        ]
-
-        for script in my_post_scripts:
-            if not script in post_scripts:
-                post_scripts.append(script)
-
-        local_context = copy.copy(self.context)
-        local_context.update({
-            'styles': styles,
-            'post_scripts': post_scripts,
-            'fixed_side_nav': True
-        })
-        return local_context
+from knotis.views import EmbeddedView
+from knotis.contrib.layout.views import DefaultBaseView
 
 
-class PrivacyView(ContextView):
-    template_name = 'knotis/privacy.html'
+class TermsAndConditionsView(EmbeddedView):
+    template_name = 'knotis/terms/terms.html'
+    default_parent_view_class = DefaultBaseView
 
     def process_context(self):
         styles = self.context.get('styles', [])
@@ -79,3 +43,41 @@ class PrivacyView(ContextView):
         return local_context
 
 
+class PrivacyView(EmbeddedView):
+    template_name = 'knotis/terms/privacy.html'
+    default_parent_view_class = DefaultBaseView
+
+    def process_context(self):
+        styles = self.context.get('styles', [])
+        post_scripts = self.context.get('post_scripts', [])
+
+        my_styles = [
+            'knotis/layout/css/global.css',
+            'knotis/layout/css/header.css',
+            'navigation/css/nav_top.css',
+            'navigation/css/nav_side.css',
+        ]
+
+        for style in my_styles:
+            if not style in styles:
+                styles.append(style)
+
+        my_post_scripts = [
+            'knotis/layout/js/layout.js',
+            'knotis/layout/js/forms.js',
+            'knotis/layout/js/header.js',
+            'navigation/js/navigation.js',
+            'knotis/api/js/api.js',
+        ]
+
+        for script in my_post_scripts:
+            if not script in post_scripts:
+                post_scripts.append(script)
+
+        local_context = copy.copy(self.context)
+        local_context.update({
+            'styles': styles,
+            'post_scripts': post_scripts,
+            'fixed_side_nav': True
+        })
+        return local_context
