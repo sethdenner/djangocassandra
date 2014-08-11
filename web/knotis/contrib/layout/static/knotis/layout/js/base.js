4cc041a9c500bcc6309ec23ca8f697656d2cee0d
David Ortiz <david@davidortiz.me>
2014-08-11 09:20:08 -0700

    Bug Fix For #100, misc requests

--- web/knotis/contrib/layout/static/knotis/layout/js/base.js
+++ web/knotis/contrib/layout/static/knotis/layout/js/base.js
@@ -3,12 +3,12 @@
 
 (function ($) {
     "use strict";
-
-    $( "#sidebar-wrapper" ).delay(1500).hover(function () {
-        $( "#sidebar-wrapper" ).animate({
+    
+    $( "#sidebar-wrapper").hover(function () {
+        $( "#sidebar-wrapper").animate({
             width: "250px",
-        },  200, "linear");
-
+        },  20, "linear");
+        
         $('.sidebar-label').fadeIn('fast');
         $('.sidebar-overlay').fadeIn('fast');
     }, function () {
@@ -20,11 +20,40 @@
 
     $('.sidebar-nav li').each(function (){
         $(this).hover(function (){
-            $(this).find('.circle').css('background', '#303030');
+            $(this).css('background', '#3b3b3b');
+            $('.sidebar-label').removeClass('sidebar-label-selected');
+            $('.sidebar-label').addClass('sidebar-label-selected');
+            $(this).find('.sidebar-label-teal').addClass('sidebar-label-selected');
+            $('.sidebar-label').css('color', '#363636');
+            $(this).find('.circle').css('background', '#363636');
+            $('.sidebar-label').addClass('sidebar-label-selected');
         }, function (){
+            $(this).css('background', '#fff');
             $(this).find('.circle').css('background', '#595959');
         });
+    });
 
+    $('.sidebar-nav li').each(function (){
+
+        $('.sidebar-nav li').css('background', '#fff');
+
+        $(this).click(function (){
+            $('.sidebar-nav li').css('background', '#FFF');
+            $(this).addClass('sidebar-li-selected');
+            
+            $(this).each(function (){
+                $(this).css('background', '#2cc397');
+                $('.sidebar-label').addClass('sidebar-label-selected');
+                $('.sidebar-label').addClass('sidebar-label-teal');
+            });
+
+            $( "#sidebar-wrapper" ).animate({
+                width: "70px",
+            },  20, "linear");
+
+            $('.sidebar-overlay').fadeOut(10);
+
+        });
     });
 
 })(jQuery);
