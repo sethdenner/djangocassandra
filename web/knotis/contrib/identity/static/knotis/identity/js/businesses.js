49493b6aa1275e457563cb9bfdc22aea3ead568a
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-10-07 21:59:16 -0700

    Changed function name and call to camelCase rather than snake_case

--- web/knotis/contrib/identity/static/knotis/identity/js/businesses.js
+++ web/knotis/contrib/identity/static/knotis/identity/js/businesses.js
@@ -4,6 +4,7 @@
             namespace:'scroll.businesses',
             url:'/businesses/grid',
             dataId:'id-establishments',
+            onDone: $.identity.initializeBusinessTiles
         });
     });
 })(jQuery);
