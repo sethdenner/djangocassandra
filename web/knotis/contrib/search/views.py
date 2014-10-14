02dabed19688f4ecd2f5020f73f5f0196b68c2cf
Sebastian Imlay <sebastian.imlay@gmail.com>
2014-10-13 18:29:46 -0700

    Removed pagination from search grid.

--- web/knotis/contrib/search/views.py
+++ web/knotis/contrib/search/views.py
@@ -27,9 +27,7 @@ class SearchResultsView(EmbeddedView):
     view_name = 'search_results'
     post_scripts = [
         'knotis/layout/js/action_button.js',
-        'knotis/layout/js/pagination.js',
         'knotis/identity/js/identity-action.js',
-        'knotis/identity/js/businesses.js',
         'knotis/identity/js/business-tile.js',
     ]
 
