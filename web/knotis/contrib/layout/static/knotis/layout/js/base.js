f3fcf1a29a48572584ca4464b983a1223eb5483f
product17 <crocker.brent@gmail.com>
2014-12-11 20:25:49 -0800

    Changing tabbing to match the rest of the file

--- web/knotis/contrib/layout/static/knotis/layout/js/base.js
+++ web/knotis/contrib/layout/static/knotis/layout/js/base.js
@@ -18,7 +18,7 @@
             };
         });
         $('#mainNavMobileToggle').trigger("change");
-	$('[data-toggle="popover"]').popover();
+        $('[data-toggle="popover"]').popover();
     });
 
 })(jQuery);
