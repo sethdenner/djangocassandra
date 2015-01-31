826f7c7c86b5e4ed261176c25a3254f3f23847f2
MDGCAT <mike@knotis.com>
2015-01-30 16:48:14 -0800

    Added 'load more' to all views that have pagination

--- web/knotis/contrib/layout/static/knotis/layout/js/pagination.js
+++ web/knotis/contrib/layout/static/knotis/layout/js/pagination.js
@@ -5,58 +5,69 @@
             url:'',
             dataId:'',
             onDone: function() {},
-            count_increment: 24
-        }
-        $.extend(options, user_options)
+            count_increment: 24,
+            infinite_scroll: false
+        };
+        $.extend(options, user_options);
 
         var page = 1;
         var count = options.count_increment;
         var results_left = true;
         var fetching_results = false;
-        var load_more_markup =  '<div class="row-fluid load-more">' +
-        '<button class="btn btn-knotis-action btn-load-more">Load More Results</button>' +
-        '</div>';
-        var no_more_markup = '<div class="row-fluid load-more">' +
-        '<button disabled class="btn btn-knotis-action btn-load-more">No More Results</button>' +
-        '</div>';
-        var scroll_space = 'scroll'
-        $(window).off(scroll_space).on(scroll_space, function(event) {
+        var load_more_markup =  'Load More Results';
+        var no_more_markup = 'No More Results';
+        var scroll_space = 'scroll';
 
-            var $this = $(this);
-
-
-            if (!results_left) {
-                $this.off(scroll_space);
-                return;
-            }
-
-            if (!fetching_results && $this.scrollTop() + $this.innerHeight() >= $(document).innerHeight() - 1000) {
-                fetching_results = true;
-                fetch_url = [
-                    options.url,
-                    ++page,
-                    count,
-                    ''
-                ].join('/');
-                $.ajax({
-                    url: fetch_url,
-                    global: false,
-                    success: function(data, status, request) {
-                        data = data.replace(/(\r\n|\n|\r)/gm,"");
-                        if (!data) {
-                            results_left = false;
-                            return;
-                        }
-
-                        var $markup = $(data);
-                        $('div[data-id=' + options.dataId + ']').append($markup);
-
-                        fetching_results = false;
-                        options.onDone();
-                        $.navigation.reinitialize();
+        var get_results = function (page_number) {
+            fetching_results = true;
+            fetch_url = [
+                options.url,
+                page,
+                count,
+                ''
+            ].join('/');
+    
+            $.ajax({
+                url: fetch_url,
+                global: false,
+                success: function(data, status, request) {
+                    data = data.replace(/(\r\n|\n|\r)/gm,"");
+                    if (!data) {
+                        results_left = false;
+                        $('#load_more_button').html(no_more_markup);
+                        return;
                     }
-                });
-            }
-        });
-    }
+
+                    var $markup = $(data);
+                    $('div[data-id=' + options.dataId + ']').append($markup);
+
+                    fetching_results = false;
+                    options.onDone();
+                    $.navigation.reinitialize();
+                }
+            });
+        };
+        
+        if (options.infinite_scroll) {
+            $(window).off(scroll_space).on(scroll_space, function(event) {
+
+                var $this = $(this);
+
+                if (!results_left) {
+                    $this.off(scroll_space);
+                    return;
+                }
+
+                if (!fetching_results && $this.scrollTop() + $this.innerHeight() >= $(document).innerHeight() - 1000) {
+                    get_results(page++);
+                }
+            });
+
+        } else {
+
+            $('#load_more_button').click(function(event) {
+                get_results(page++);
+            });
+        }
+    };    
 })(jQuery);
