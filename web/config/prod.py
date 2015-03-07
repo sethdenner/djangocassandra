02358eff2d2b1c314ddbb54b12a5276352ced17c
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-03-07 09:13:57 -0800

    Updated where cassandra points

--- web/config/prod.py
+++ web/config/prod.py
@@ -4,7 +4,7 @@ DATABASES = {
         'NAME': 'knotis', # Or path to database file if using sqlite3.
         'USER': '', # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '10.168.1.14', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
