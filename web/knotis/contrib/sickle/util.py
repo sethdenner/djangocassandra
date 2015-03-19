f1443d8a767a6e0d7678adfb9721696412816d7a
Seth Denner <seth@knotis.com>
2015-03-19 16:22:38 -0700

    [Finish #90556074] - Improve Image Assets In XAPI
    * Added tile_image_small and tile_image_large urls to all identity and offer resource requests in the XAPI
    * Not sure if I nailed getting you the right resolutions. Let me know if futher tweaking is involved.

--- web/knotis/contrib/sickle/util.py
+++ web/knotis/contrib/sickle/util.py
@@ -26,3 +26,52 @@ def generate_sorl_crop_string(
 
     else:
         return 'noop'
+
+
+def transform_crop_aspect_ratio(
+    new_ratio,
+    old_left,
+    old_top,
+    old_width,
+    old_height
+):
+    if (
+        None is old_left or
+        None is old_top or
+        None is old_width or
+        None is old_height
+    ):
+        return (
+            None,
+            None,
+            None,
+            None
+        )
+
+    old_ratio = (old_width + 0.) / old_height
+    if new_ratio > old_ratio:
+        new_height = int(old_width / new_ratio)
+        new_width = old_width
+
+    else:
+        new_width = int(old_height * new_ratio)
+        new_height = old_height
+
+    width_delta = new_width - old_width
+    grow_left = (width_delta / 2)
+    new_left = old_left - grow_left
+    if new_left < 0:
+        new_left = 0
+
+    height_delta = new_height - old_height
+    grow_top = (height_delta / 2)
+    new_top = old_top - grow_top
+    if new_top < 0:
+        new_top = 0
+
+    return (
+        new_left,
+        new_top,
+        new_width,
+        new_height
+    )
