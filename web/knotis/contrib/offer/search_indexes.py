c0f2eadf59af1ca98951d3199058409f9dea5892
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-04-10 16:56:35 -0700

    [Finish #92268916] Made Offers and Passport books sort by location.

--- web/knotis/contrib/offer/search_indexes.py
+++ web/knotis/contrib/offer/search_indexes.py
@@ -1,6 +1,9 @@
 import datetime
 from haystack import indexes
-from models import Offer
+from models import (
+    Offer,
+    OfferCollectionItem
+)
 
 
 class OfferIndex(indexes.SearchIndex, indexes.Indexable):
@@ -9,12 +12,34 @@ class OfferIndex(indexes.SearchIndex, indexes.Indexable):
     pub_date = indexes.DateTimeField(model_attr='pub_date')
     # Haystack is busted.
     # https://github.com/django-haystack/django-haystack/issues/866#issuecomment-27708342
-    available = indexes.BooleanField(model_attr='active', indexed=False)
+    active = indexes.BooleanField(model_attr='active', indexed=False)
+    available = indexes.BooleanField(model_attr='available', indexed=False)
+    published = indexes.BooleanField(model_attr='published', indexed=False)
+    completed = indexes.BooleanField(model_attr='completed', indexed=False)
 
     location = indexes.LocationField(model_attr='get_location')
 
     def get_model(self):
         return Offer
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(
+            pub_date__lte=datetime.datetime.now()
+        )
+
+
+class OfferCollectionItemIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    offer_collection = indexes.CharField(
+        model_attr='offer_collection',
+        null=True
+    )
+    pub_date = indexes.DateTimeField(model_attr='pub_date')
+    location = indexes.LocationField(model_attr='get_location')
+
+    def get_model(self):
+        return OfferCollectionItem
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
