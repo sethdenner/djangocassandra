9a68b563a75f938bc47aaa030a10195f55de01c5
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-04-10 15:55:22 -0700

    Pulled in build scripts from other branch.

--- setup/docker/start.sh
+++ setup/docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sleep 3
-./venv/bin/python web/manage.py syncdb --noinput
-./venv/bin/python web/manage.py collectstatic --noinput
+chmod -R o+rw ${install_location}
+
+./venv/bin/python web/manage.py runserver 0.0.0.0:8000 & disown
+service apache2 start & disown
+
+tail -f /var/log/apache2/access.log
