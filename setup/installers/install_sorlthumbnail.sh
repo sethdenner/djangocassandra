d4c601dbf5b770400c0cdd28d41d84f8b46780c4
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-04-16 03:19:53 -0700

    Fixed scripts so they don't install django1.8

--- setup/installers/install_sorlthumbnail.sh
+++ setup/installers/install_sorlthumbnail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'installing sorl-thumbnail ...'
-sorlthumbnail_git=git+https://github.com/simlay/sorl-thumbnail.git@fix_cropping_django_1.3
+sorlthumbnail_git=https://github.com/simlay/sorl-thumbnail.git@fix_cropping_django_1.3
 
 ${install_location}/venv/bin/pip install git+${sorlthumbnail_git}
