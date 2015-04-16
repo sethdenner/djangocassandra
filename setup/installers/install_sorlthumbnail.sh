f2cb0ccd4d755881c25dc390e886795eaec52e16
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-04-16 02:46:47 -0700

    Updated Dockerfile and install scripts

--- setup/installers/install_sorlthumbnail.sh
+++ setup/installers/install_sorlthumbnail.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo 'installing sorl-thumbnail ...'
-sorlthumbnail_git=https://github.com/Knotis/sorl-thumbnail.git@fix_cropping
+sorlthumbnail_git=git+https://github.com/simlay/sorl-thumbnail.git@fix_cropping_django_1.3
+
 ${install_location}/venv/bin/pip install git+${sorlthumbnail_git}
